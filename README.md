a4c61d34b917d7d9c59e1fe902e5d1e28ab60943
Kai Yuan Neo <kai@rocketacademy.co>
2021-01-15 14:43:05 +0800

    refactor script to update eslint for jsx for react repos, update docs to install later version of bash, fix git config

--- README.md
+++ README.md
@@ -6,10 +6,12 @@ Create repos from a template using GitHub command line.
 
 #### Mac Only.
 
-#### Install GitHub command line:
+#### Install Bash and GitHub command line for script:
+
+Need later version of Bash than MacOS default for associative arrays.
 
 ```
-brew install gh
+brew install bash gh
 ```
 
 #### Set configs for GitHub's new default branch name:
@@ -21,6 +23,8 @@ git config --global init.defaultBranch main
 #### Know which template repo you want to copy from.
 
 # Run the command:
+
+We need to run the script from outside `repo-template-script` because the script creates a new local repo in the folder where the script is executed.
 
 ```
 $ ./repo-template-script/create-repo.sh new-repo-name template-repo-name
@@ -41,46 +45,55 @@ If you want to create a new repo type add a key and value to the hash.
 ### Recipes
 
 basic node cli repo
+
 ```
 $ ./repo-template-script/create-repo.sh <SOME_NAME> base-node-swe1-template
 ```
 
 basic CRUD express repo - installs express
+
 ```
 $ ./repo-template-script/create-repo.sh <SOME_NAME> base-node-swe1-template express-basic
 ```
 
 CRUD express EJS
+
 ```
 $ ./repo-template-script/create-repo.sh <SOME_NAME> base-node-swe1-template express-ejs
 ```
 
 CRUD express PG
+
 ```
 $ ./repo-template-script/create-repo.sh <SOME_NAME> base-node-swe1-template express-pg
 ```
 
 basic bootstrap repo (has empty html / css files / link / script)
+
 ```
 $ ./repo-template-script/create-repo.sh <SOME_NAME> base-bootstrap-swe1-template
 ```
 
 basic html / css swe101 repo (no files)
+
 ```
 $ ./repo-template-script/create-repo.sh <SOME_NAME> base-css-html-swe1-template
 ```
 
 MVC / sequelize
+
 ```
 $ ./repo-template-script/create-repo.sh <SOME_NAME> base-mvc-swe1-template mvc
 ```
 
 React MVC
+
 ```
 $ ./repo-template-script/create-repo.sh <SOME_NAME> base-react-swe1-template react
 ```
 
 React Reload MVC
+
 ```
 $ ./repo-template-script/create-repo.sh <SOME_NAME> base-react-swe1-template react-reload
 ```
--- create-repo.sh
+++ create-repo.sh
@@ -1,141 +1,190 @@
 #!/usr/local/bin/bash
 
+# configure git to use main as default branch
+git config --global init.defaultBranch main
+
+# declare PACKAGE_LISTS and DEV_PACKAGE_LISTS as associative arrays
 declare -A PACKAGE_LISTS
+declare -A DEV_PACKAGE_LISTS
 
 PACKAGE_LISTS=(
-  ["express-basic"]="express"
-  ["express-ejs"]="express ejs"
-  ["express-pg"]="express ejs pg"
-  ["mvc"]="express ejs pg sequelize method-override cookie-parser"
+  ["express-basic"]=" \
+    express \
+  "
+  ["express-ejs"]=" \
+    ejs \
+    express \
+  "
+  ["express-pg"]=" \
+    ejs \
+    express \
+    pg \
+  "
+  ["mvc"]=" \
+    cookie-parser \
+    ejs \
+    express \
+    method-override \
+    pg \
+    sequelize \
+  "
   # TODO: mvc webpack
-  ["react"]="express ejs pg sequelize method-override cookie-parser"
-  ["react-reload"]="express ejs pg sequelize method-override cookie-parser"
+  ["react"]=" \
+    cookie-parser \
+    ejs \
+    express \
+    method-override \
+    pg \
+    sequelize \
+  "
+  ["react-reload"]=" \
+    cookie-parser \
+    ejs \
+    express \
+    method-override \
+    pg \
+    sequelize \
+  "
 )
 
 DEV_PACKAGE_LISTS=(
-  ["express-basic"]="nodemon"
-  ["express-ejs"]="nodemon"
-  ["express-pg"]="nodemon"
-  ["mvc"]="nodemon sequelize-cli faker"
+  ["express-basic"]=" \
+    nodemon \
+  "
+  ["express-ejs"]=" \
+    nodemon \
+  "
+  ["express-pg"]=" \
+    nodemon \
+  "
+  ["mvc"]=" \
+    faker \
+    nodemon \
+    sequelize-cli \
+  "
   # TODO: mvc webpack
-  ["react"]="nodemon \
-             sequelize-cli \
-             faker \
-             webpack \
-             webpack-cli \
-             webpack-merge \
-             @babel/core \
-             @babel/preset-env \
-             @babel/preset-react \
-             babel-loader \
-             css-loader \
-             html-webpack-plugin \
-             mini-css-extract-plugin \
-             sass \
-             sass-loader \
-             axios \
-             webpack-dev-middleware \
-             webpack-dev-server \
-             react \
-             react-dom"
-
-
-  ["react-reload"]="eslint-plugin-react \
-                    eslint-plugin-react-hooks \
-                    nodemon \
-                    sequelize-cli \
-                    faker \
-                    webpack \
-                    webpack-cli \
-                    webpack-merge \
-                    @babel/core \
-                    @babel/preset-env \
-                    @babel/preset-react \
-                    babel-loader \
-                    css-loader \
-                    html-webpack-plugin \
-                    mini-css-extract-plugin \
-                    sass \
-                    sass-loader \
-                    axios \
-                    @pmmmwh/react-refresh-webpack-plugin \
-                    react-refresh \
-                    webpack-dev-middleware \
-                    webpack-hot-middleware \
-                    webpack-dev-server \
-                    react \
-                    react-dom"
+  ["react"]=" \
+    @babel/core \
+    @babel/preset-env \
+    @babel/preset-react \
+    axios \
+    babel-loader \
+    css-loader \
+    eslint-config-airbnb \
+    eslint-plugin-jsx-a11y \
+    eslint-plugin-react \
+    eslint-plugin-react-hooks \
+    faker \
+    html-webpack-plugin \
+    mini-css-extract-plugin \
+    nodemon \
+    react \
+    react-dom \
+    sass \
+    sass-loader \
+    sequelize-cli \
+    webpack \
+    webpack-cli \
+    webpack-dev-middleware \
+    webpack-dev-server \
+    webpack-merge \
+  "
+  ["react-reload"]=" \
+    @babel/core \
+    @babel/preset-env \
+    @babel/preset-react \
+    @pmmmwh/react-refresh-webpack-plugin \
+    axios \
+    babel-loader \
+    css-loader \
+    eslint-config-airbnb \
+    eslint-plugin-jsx-a11y \
+    eslint-plugin-react \
+    eslint-plugin-react-hooks \
+    faker \
+    html-webpack-plugin \
+    mini-css-extract-plugin \
+    nodemon \
+    react \
+    react-dom \
+    react-refresh \
+    sass \
+    sass-loader \
+    sequelize-cli \
+    webpack \
+    webpack-cli \
+    webpack-dev-middleware \
+    webpack-dev-server \
+    webpack-hot-middleware \
+    webpack-merge \
+  "
 )
 
 set -e # exit if anything goes wrong
-
 echo "Running..."
 
 if [[ $# -ge 2 ]]; then # test for two arguments
+  # use variables for the arguments
+  REPO_NAME=$1
+  TEMPLATE_NAME=$2
 
-    # use variables for the arguments
-    REPO_NAME=$1
-    TEMPLATE_NAME=$2
+  echo "Creating a repo named: $REPO_NAME from template: $TEMPLATE_NAME"
 
-    echo "Creating a repo named: $REPO_NAME from template: $TEMPLATE_NAME"
+  # run the gh cli command
+  gh repo create rocketacademy/$REPO_NAME --public --template rocketacademy/$TEMPLATE_NAME
+  # gh creates a new dir. cd inside
+  cd $REPO_NAME
+  # set git pull config to remove warning when pulling
+  git config pull.rebase false
+  # get any files made from the template
+  git pull origin main
+  # create the new npm files
+  npm init -y
+  # install the editor dependencies
+  npm i -D eslint eslint-config-airbnb-base eslint-plugin-import
 
-    # run the gh cli command
-    gh repo create rocketacademy/$REPO_NAME --public --template rocketacademy/$TEMPLATE_NAME
+  # if it's for node, we need "type": "module" for import
+  if [ "${TEMPLATE_NAME}" = 'base-node-swe1-template' ]; then
+    # add module type for import syntax
+    sed -i '' -e '$ d' package.json
+    echo ',"type": "module"}' >> package.json
+  fi
 
-    # gh creates a new dir
-    # cd inside
-    cd $REPO_NAME
+  # install repo-type-specific dependencies
+  if [ "${TEMPLATE_NAME}" = 'base-mvc-swe1-template' ] || [ "${TEMPLATE_NAME}" = 'base-node-swe1-template' ] || [ "${TEMPLATE_NAME}" = 'base-react-swe1-template' ]; then
 
-    # get any files made from the template
-    git pull origin main
+    if [[ $# -eq 3 ]]; then # test for a third arg
+      echo "template name:"
+      echo $TEMPLATE_NAME
 
-    # the dir doesnt have the gh files yet. pull them down
-    #git pull origin main
+      NODE_REPO_TYPE=$3
+      echo "node repo type:"
+      echo $NODE_REPO_TYPE
 
-    # create the new npm files
-    npm init -y
+      echo "packages:"
+      echo ${PACKAGE_LISTS[$NODE_REPO_TYPE]}
 
-    # install the editor dependencies
-    npm i -D eslint eslint-config-airbnb-base eslint-plugin-import
+      deps="npm i ${PACKAGE_LISTS[$NODE_REPO_TYPE]}"
+      echo "installing deps"
+      echo $deps
+      eval "$deps"
 
-    # if it's for node, we need type : module for import
-    if [ "${TEMPLATE_NAME}" = 'base-node-swe1-template' ]; then
-      # add module type for import syntax
-      sed -i '' -e '$ d' package.json
-      echo ',"type":"module"}' >> package.json
-    fi
+      devDeps="npm i -D ${DEV_PACKAGE_LISTS[$NODE_REPO_TYPE]}"
+      echo "installing DEV deps"
+      echo $devDeps
+      eval "$devDeps"
+    fi # otherwise its the default
 
-    if [ "${TEMPLATE_NAME}" = 'base-mvc-swe1-template' ] || [ "${TEMPLATE_NAME}" = 'base-node-swe1-template' ] || [ "${TEMPLATE_NAME}" = 'base-react-swe1-template' ]; then
+  # later we can make other mods for other repo types
+  elif [ "${TEMPLATE_NAME}" = 'swe101' ]; then
+    echo "creating swe101 repo"
+  fi
 
-      if [[ $# -eq 3 ]]; then # test for a third arg
-        echo "template name:"
-        echo TEMPLATE_NAME
-
-        NODE_REPO_TYPE=$3
-
-        deps="npm i ${PACKAGE_LISTS[$NODE_REPO_TYPE]}"
-        echo "installing deps"
-        echo $deps
-        eval "$deps"
-
-        devDeps="npm i -D ${DEV_PACKAGE_LISTS[$NODE_REPO_TYPE]}"
-        echo "installing DEV deps"
-        echo $devDeps
-        eval "$devDeps"
-
-      fi # otherwise its the default
-
-    # later we can make other mods for other repo types
-    elif [ "${TEMPLATE_NAME}" = 'swe101' ]; then
-      echo "creating swe101 repo"
-    fi
-
-    # add and push the new files
-
-    git add .
-    git commit -m "add package.json"
-    git push origin main
+  # add and push the new files
+  git add .
+  git commit -m "add package.json and package-lock.json"
+  git push origin main
 
 else
-    echo "Wrong number of arguments: ./create-repo.sh repo-name template-name"
+  echo "Wrong number of arguments: ./create-repo.sh repo-name template-name"
 fi
